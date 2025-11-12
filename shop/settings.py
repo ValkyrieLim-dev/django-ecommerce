aace1a09c45266ef1cd59f20e63845facbb44cd6
ValkyrieLim-dev <kennethalvinlim@gmail.com>
2025-11-13 04:32:33 +0800

    Configure Cloudinary for media

--- shop/settings.py
+++ shop/settings.py
@@ -89,3 +89,22 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # --- Default primary key field type ---
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cloudinary settings
+INSTALLED_APPS += ['cloudinary', 'cloudinary_storage']
+
+# Use Cloudinary for media files
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# Your Cloudinary credentials
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'dwo2okdbu',
+    'API_KEY': '415216595646991',
+    'API_SECRET': 'YB9GTKCv9DBgs8j1KKegtBsJvEo',
+}
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
