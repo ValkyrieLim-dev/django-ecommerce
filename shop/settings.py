d2fb81dfb6f43f3bfd11a36a684272ee36c88c04
ValkyrieLim-dev <kennethalvinlim@gmail.com>
2025-11-13 04:21:13 +0800

    Collect static files for Render

--- shop/settings.py
+++ shop/settings.py
@@ -78,7 +78,7 @@ USE_TZ = True
 
 # --- Static files (CSS, JS, Images) ---
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Enable Whitenoise compression
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
