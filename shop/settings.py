813cb84f02689a1f288e3405156e7677f5f46698
ValkyrieLim-dev <kennethalvinlim@gmail.com>
2025-11-13 10:15:53 +0800

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # --- Base directory ---
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,12 +57,8 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 # --- Database ---
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
-
 # --- Password validation ---
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
